43553b7fc2f4625aa4f139dab8fac91e91d83142
Philani Ncube <ncbphi001@gmail.com>
2022-12-05 17:02:49 +0200

    pricing

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,8 +1,12 @@
+import BottomHero from "./BottomHero";
 import Hero from "./Hero";
+import PricingSection from "./PricingSection";
 
 const page = () => {
   return <>
           <Hero />
+          <PricingSection />
+          <BottomHero />
          </>;
 };
 export default page;
