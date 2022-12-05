fe0a41b17345f49f6e01916b18f89016844b2ec0
Philani Ncube <ncbphi001@gmail.com>
2022-12-05 17:02:16 +0200

    Pricing section

---
+++ app/pricing/PricingSection.tsx
@@ -0,0 +1,10 @@
+const PricingSection = () => {
+  return <div className="min-h-screen bg-white">
+    <div className="max-w-7xl mx-auto px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+
+      </div>
+    </div>
+  </div>;
+};
+export default PricingSection;
